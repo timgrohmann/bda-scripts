47f693fafbfede4402cd9ad55f259de51f478931
Tim Grohmann <timgrohmann@icloud.com>
2020-11-04 11:08:33 +0100

    added kafka producer script

---
+++ producer.py
@@ -0,0 +1,10 @@
+import kafka
+
+producer = kafka.KafkaProducer()
+
+dataset = open('data/frankfurt a.m.-goethestraße-20180930-20201031-day.csv', encoding='utf-8')
+lines = dataset.readlines()
+
+for i, line in enumerate(lines):
+    print(line)
+    producer.send('peoplecount', value=bytearray(line, encoding='utf-8'), key=bytearray(str(i), encoding='utf-8'))
