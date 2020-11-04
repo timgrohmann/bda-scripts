60da3780aceb9622aa7b7b624251d921dd799bde
Tim Grohmann <timgrohmann@icloud.com>
2020-11-04 15:31:26 +0100

    updated consumer

--- consumer.py
+++ consumer.py
@@ -1,20 +1,22 @@
 import kafka
+import datetime as dt
 from pymongo import MongoClient
 
 consumer = kafka.KafkaConsumer('peoplecount')
 
 client = MongoClient()
 pp = client['bigdata']['peoplecount']
+pp.delete_many({})
 
 for message in consumer:
     print('inserting new message')
     values = message.value.decode('utf-8').split(';')
     pp.insert_one({
         'place': values[0],
-        'timestamp': values[1],
+        'day': dt.datetime.strptime(values[1][:-6],"%Y-%m-%d %H:%M:%S"),
         'weekday': values[2],
-        'count': values[3],
-        'temp': values[4],
+        'count': int(values[3]),
+        'temp': int(values[4]) if values[4] != '' else None,
         'weather': values[5],
     })
     
