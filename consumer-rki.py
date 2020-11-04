aed866bf21a9fdbe3cc16f37977d639e89478882
Niclas Kaufmann <kaufmann.niclas@web.de>
2020-11-04 20:25:36 +0100

    added consumer and producer for corona data

---
+++ consumer-rki.py
@@ -0,0 +1,39 @@
+import kafka
+from pymongo import MongoClient
+import csv
+from datetime import datetime
+
+consumer = kafka.KafkaConsumer('rki')
+
+client = MongoClient()
+pp = client['bigdata']['corona']
+
+for message in consumer:
+    print('incoming message')
+    values = list(csv.reader([message.value.decode('utf-8')], delimiter=','))[0]
+
+    # print(values)
+    try:
+        pp.insert_one({
+            'IdBundesland': int(values[0]),
+            'Bundesland': values[1],
+            'Landkreis': values[2],
+            'Altersgruppe': values[3],
+            'Geschlecht': values[4],
+            'AnzahlFall': int(values[5]),
+            'AnzahlTodesfall': int(values[6]),
+            'ObjectId': values[7],
+            'IdLandkreis': values[8],
+            'Altersgruppe2': values[9],
+            'Refdatum': datetime.fromtimestamp(int(values[10]) // 1000),
+            'Meldedatum': datetime.fromtimestamp(int(values[11]) // 1000),
+            'IstErkrankungsbeginn': int(values[12]),
+            'NeuerFall': int(values[13]),
+            'NeuerTodesfall': int(values[14]),
+            'NeuGenesen': (values[15]),
+            'AnzahlGenesen': int(values[16]),
+            'Datenstand': values[17]
+        })
+    except Exception as e:
+        print(e)
+        print(values)
---
+++ producer-rki.py
@@ -0,0 +1,11 @@
+import kafka
+
+producer = kafka.KafkaProducer()
+
+# dataset = open('data\RKI_COVID19_small.csv', encoding='utf-8')
+dataset = open('data\RKI_COVID19.csv', encoding='utf-8')
+lines = dataset.readlines()[1:]
+
+for i, line in enumerate(lines):
+    producer.send('rki', value=bytearray(line, encoding='utf-8'), key=bytearray(str(i), encoding='utf-8'))
+
