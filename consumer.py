47f2bf00fc565dadcb29f66f03a764ee9beaa11e
Tim Grohmann <timgrohmann@icloud.com>
2020-11-08 11:58:48 +0100

    added visualization

--- consumer.py
+++ consumer.py
@@ -8,14 +8,30 @@ client = MongoClient()
 pp = client['bigdata']['peoplecount']
 pp.delete_many({})
 
+sums = {}
+
 for message in consumer:
     print('inserting new message')
     values = message.value.decode('utf-8').split(';')
+    place = values[0]
+    count = int(values[3])
+    if count == 0:
+        if place not in sums:
+            continue
+        else:
+            val = sums[place]
+            count = round(float(val['total'])/val['count'])
+    else:
+        if place not in sums:
+            sums[place] = {'count': 1, 'total': count}
+        else:
+            sums[place]['count'] += 1
+            sums[place]['total'] += count
     pp.insert_one({
-        'place': values[0],
+        'place': place,
         'day': dt.datetime.strptime(values[1][:-6],"%Y-%m-%d %H:%M:%S"),
         'weekday': values[2],
-        'count': int(values[3]),
+        'count': count,
         'temp': int(values[4]) if values[4] != '' else None,
         'weather': values[5],
     })
--- producer.py
+++ producer.py
@@ -2,9 +2,19 @@ import kafka
 
 producer = kafka.KafkaProducer()
 
-dataset = open('data/frankfurt a.m.-goethestraße-20180930-20201031-day.csv', encoding='utf-8')
-lines = dataset.readlines()[1:]
+paths = [
+    #'data/berlin-kurfürstendamm nordseite (ost)-20181109-20201031-day.csv',
+    'data/frankfurt a.m.-goethestraße-20180930-20201031-day.csv',
+    'data/stuttgart-königstraße (mitte)-20180930-20201031-day.csv',
+    #'data/karlsruhe-kaiserstraße (mitte)-20181212-20201031-day.csv',
+    'data/düsseldorf-königsallee ostseite (süd)-20180930-20201031-day.csv',
+    'data/frankfurt a.m.-große bockenheimer straße-20180930-20201031-day.csv'
+]
 
-for i, line in enumerate(lines):
-    print(line)
-    producer.send('peoplecount', value=bytearray(line, encoding='utf-8'), key=bytearray(str(i), encoding='utf-8'))
+for path in paths:
+    dataset = open(path, encoding='utf-8')
+    lines = dataset.readlines()[1:]
+
+    for i, line in enumerate(lines):
+        print(line)
+        producer.send('peoplecount', value=bytearray(line, encoding='utf-8'), key=bytearray(str(i), encoding='utf-8'))
